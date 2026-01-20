03068c1987db9d821e35dc89182a2f0c97efb7d7
Matheus Ernesto Dos Santos <73611965+Matheus-Ernesto@users.noreply.github.com>
2026-01-20 10:35:32 -0300

    Update engine.cpp

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,7 +1,8 @@
-// 1.5 hours spent
+// 3 hours spent
 // OpenGL choosed
 #include <stdio.h>
 #include <iostream>
+#include <chrono>
 #include <GL/glut.h>
 
 using namespace std;
@@ -39,26 +40,47 @@ class ConfigLoader{
 
 class Engine{
 public:
+    static int totalFrames;
+    static std::chrono::high_resolution_clock::time_point last;
 
-    void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-	glutSwapBuffers();
+    static void display() {
+        totalFrames++;
+        int fps = 0;
+
+        auto now = std::chrono::high_resolution_clock::now();
+
+        auto diff = std::chrono::duration<double>(now - last).count();
+
+        if(diff >= 1.0){
+            fps = totalFrames / 60;
+            totalFrames = 0;
+            last = now;
+            string fpsTotal = "FPS" + to_string(fps);
+            glutSetWindowTitle(fpsTotal.c_str());
+        }
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	    glutSwapBuffers();
+        glutPostRedisplay();
     }
 
-    void start() {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("Test");
-	glutDisplayFunc(display);
-	glutMainLoop(); 
+    void start(int argc, char* argv[]) {
+	    glutInit(&argc, argv);
+	    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	    glutCreateWindow("Test");
+	    glutDisplayFunc(display);
+	    glutMainLoop(); 
     }
 };
 
-int main()
+int Engine::totalFrames = 0;
+std::chrono::high_resolution_clock::time_point Engine::last = std::chrono::high_resolution_clock::now();
+
+int main(int argc, char* argv[])
 {
     std::cout<<"Engine launch\n";
     std::cout<<"Window launch\n";
     Engine engine;
-    engine.start();
+    engine.start(argc, argv);
     return 0;
 }
