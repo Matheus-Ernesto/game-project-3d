829d7e865037fd621a1954a87dc530d3a5cc263f
Matheus-Ernesto <matheusernestodossantos231@gmail.com>
2026-02-17 13:25:49 -0300

    Problems with culling

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -302,40 +302,186 @@ public:
         textures.clear();
     }
 };
-/*
+
+class Model
+{
+};
+
+#include <vector>
+using namespace std;
+
+class Texture
+{
+public:
+    float r = 1.f, g = 1.f, b = 1.f, a = 1.f;
+
+    static constexpr int WHITEBONE = 0;
+    static constexpr int GREY = 1;
+    static constexpr int RED = 2;
+    static constexpr int BLUE = 3;
+    static constexpr int GREEN = 4;
+    static constexpr int GLASSBLUE = 5;
+    static constexpr int OTHER = 6;
+
+    void setColor(int type)
+    {
+
+        switch (type)
+        {
+        case WHITEBONE:
+            r = 0.8f;
+            g = 0.8f;
+            b = 0.9f;
+            a = 1.f;
+            break;
+
+        case GREY:
+            r = 0.2f;
+            g = 0.2f;
+            b = 0.2f;
+            a = 1.f;
+            break;
+
+        case RED:
+            r = 1.f;
+            g = 0.f;
+            b = 0.f;
+            a = 1.f;
+            break;
+
+        case BLUE:
+            r = 0.f;
+            g = 0.f;
+            b = 1.f;
+            a = 1.f;
+            break;
+
+        case GREEN:
+            r = 0.f;
+            g = 1.f;
+            b = 0.f;
+            a = 1.f;
+            break;
+
+        case GLASSBLUE:
+            r = 0.2f;
+            g = 0.6f;
+            b = 1.f;
+            a = 0.5f;
+            break;
+
+        case OTHER:
+            r = 0.5f;
+            g = 0.5f;
+            b = 0.5f;
+            a = 1.f;
+            break;
+
+        default:
+            r = 1.f;
+            g = 1.f;
+            b = 1.f;
+            a = 1.f;
+            break;
+        }
+    }
+
+    vector<float> getColor() const
+    {
+        return {r, g, b, a};
+    }
+};
 
 class Object3D
 {
 public:
-    var Axis;
-    var Texture;
-    var Visible;
+    float pos_x = 0.f, pos_y = 0.f, pos_z = 0.f;
+    float rot_x = 0.f, rot_y = 0.f, rot_z = 0.f;
+    float sca_x = 1.f, sca_y = 1.f, sca_z = 1.f;
 
-    void move(Object obj);
-    void hitted(Object);
-}
+    Texture texture;
+    bool visible = true;
+    string name = "";
 
-class Object2D
-{
-public:
-    var Axis;
-    var Texture;
-    var Visible;
+    Model model;
 
-    void move(Object obj);
-    void hitted(Object);
-}
+    int type = 0;
+    static constexpr int CUBE = 0;
+    static constexpr int MODEL = 1;
 
-class HitBox
-{
-public:
-    var Axis;
-    var Size;
-    var Visible;
+    void draw()
+    {
+        GLfloat mat_diffuse[] = {texture.r, texture.g, texture.b, texture.a};
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
 
-    void move(Object obj);
-}
-*/
+        GLfloat mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; 
+        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+
+        GLfloat mat_shininess[] = {20.0f}; // Brilho
+        glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+        switch (type)
+        {
+        case CUBE:
+            glPushMatrix();
+            glTranslatef(pos_x, pos_y, pos_z);
+
+            glRotatef(rot_x, 1.f, 0.f, 0.f);
+            glRotatef(rot_y, 0.f, 1.f, 0.f);
+            glRotatef(rot_z, 0.f, 0.f, 1.f);
+
+            glScalef(sca_x, sca_y, sca_z);
+
+            glBegin(GL_QUADS);
+
+            glColor4f(texture.r, texture.g, texture.b, texture.a);
+
+            glNormal3f(0.f, 0.f, 1.f);
+            glVertex3f(-1.f, -1.f, 1.f);
+            glVertex3f(1.f, -1.f, 1.f);
+            glVertex3f(1.f, -1.f, -1.f);
+            glVertex3f(-1.f, -1.f, -1.f);
+
+            /*
+
+            glVertex3f(-1.f, -1.f, -1.f);
+            glVertex3f(1.f, -1.f, -1.f);
+            glVertex3f(1.f, 1.f, -1.f);
+            glVertex3f(-1.f, 1.f, -1.f);
+
+            glVertex3f(-1.f, -1.f, 1.f);
+            glVertex3f(-1.f, 1.f, 1.f);
+            glVertex3f(1.f, 1.f, 1.f);
+            glVertex3f(1.f, -1.f, 1.f);
+
+            glVertex3f(1.f, -1.f, -1.f);
+            glVertex3f(1.f, 1.f, -1.f);
+            glVertex3f(1.f, 1.f, 1.f);
+            glVertex3f(1.f, -1.f, 1.f);
+
+            glVertex3f(-1.f, 1.f, -1.f);
+            glVertex3f(-1.f, 1.f, 1.f);
+            glVertex3f(1.f, 1.f, 1.f);
+            glVertex3f(1.f, 1.f, -1.f);
+
+            glVertex3f(-1.f, -1.f, -1.f);
+            glVertex3f(-1.f, -1.f, 1.f);
+            glVertex3f(1.f, -1.f, 1.f);
+            glVertex3f(1.f, -1.f, -1.f);
+
+            */
+
+            glEnd();
+
+            glPopMatrix();
+            break;
+        case MODEL:
+            break;
+        default:
+            break;
+        }
+    }
+};
+
 class Camera
 {
 public:
@@ -344,18 +490,63 @@ public:
     float fov, limitNear, limitFar;
 };
 
-class Scene{
+class Scene
+{
+public:
+    vector<Object3D> objects3d;
 
+    void add(Object3D obj)
+    {
+        objects3d.push_back(obj);
+    }
+
+    void draw()
+    {
+        glEnable(GL_LIGHTING); // Habilita o sistema de iluminação
+        glEnable(GL_LIGHT0);   // Habilita a luz 0
+
+        GLfloat light_position[] = {0.0f, 3.0f, 2.0f, 1.0f}; // Posição da luz
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+        GLfloat light_diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Cor da luz (branca)
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+
+        GLfloat light_specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Reflexão especular
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+        GLfloat light_ambient[] = {0.2f, 0.2f, 0.2f, 1.0f}; // Luz ambiente (suave)
+        glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+
+        for (auto &obj : objects3d)
+        {
+            if (obj.visible)
+                obj.draw();
+        }
+    }
+
+    Object3D &getObject3D(string name)
+    {
+        for (auto &obj : objects3d)
+        {
+            if (obj.name == name)
+                return obj;
+        }
+        static Object3D invalidObject;
+        cout << "err Obj " << name << "not found\n";
+        return invalidObject;
+    }
 };
 
 class World
 {
 public:
     float colorBackgroud_r, colorBackgroud_g, colorBackgroud_b;
+    Scene scene;
 
     Camera camera;
 
-    World() {
+    World()
+    {
         camera.x = 0.f;
         camera.y = 0.f;
         camera.z = -10.f;
@@ -370,57 +561,22 @@ public:
         colorBackgroud_b = 0.f;
     }
 
-    void setBackgroundColor(float r, float g, float b){
+    void setBackgroundColor(float r, float g, float b)
+    {
         colorBackgroud_r = r;
         colorBackgroud_g = g;
         colorBackgroud_b = b;
     }
 
-    void drawBackground(){
+    void drawBackground()
+    {
         glClearColor(colorBackgroud_r, colorBackgroud_g, colorBackgroud_b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
     }
 
-    void draw(){
-        glBegin(GL_QUADS);
-
-        glColor3f(0.f, 1.f, 1.f);
-        glVertex3f(-1.f, -1.f, -1.f);
-        glVertex3f(1.f, -1.f, -1.f);
-        glVertex3f(1.f, -1.f, 1.f);
-        glVertex3f(-1.f, -1.f, 1.f);
-
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-1.f, -1.f, 1.f);
-        glVertex3f(1.f, -1.f, 1.f);
-        glVertex3f(1.f, 1.f, 1.f);
-        glVertex3f(-1.f, 1.f, 1.f);
-        
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(-1.f, -1.f, -1.f);
-        glVertex3f(-1.f, 1.f, -1.f);
-        glVertex3f(1.f, 1.f, -1.f);
-        glVertex3f(1.f, -1.f, -1.f);
-
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(-1.f, -1.f, -1.f);
-        glVertex3f(-1.f, -1.f, 1.f);
-        glVertex3f(-1.f, 1.f, 1.f);
-        glVertex3f(-1.f, 1.f, -1.f);
-
-        glColor3f(1.f, 1.f, 0.f);
-        glVertex3f(1.f, -1.f, -1.f);
-        glVertex3f(1.f, 1.f, -1.f);
-        glVertex3f(1.f, 1.f, 1.f);
-        glVertex3f(1.f, -1.f, 1.f);
-
-        glColor3f(1.f, 0.f, 1.f);
-        glVertex3f(-1.f, 1.f, -1.f);
-        glVertex3f(-1.f, 1.f, 1.f);
-        glVertex3f(1.f, 1.f, 1.f);
-        glVertex3f(1.f, 1.f, -1.f);
-
-        glEnd();
+    void draw()
+    {
+        scene.draw();
     };
 };
 
@@ -430,13 +586,8 @@ public:
     World world;
     bool enable3D;
 
-    void draw(sf::RenderWindow & window){
-        
-        glEnable(GL_DEPTH_TEST);
-        glDepthMask(GL_TRUE);
-
-        world.drawBackground();
-
+    void draw(sf::RenderWindow &window)
+    {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glMatrixMode(GL_PROJECTION);
@@ -447,21 +598,26 @@ public:
         glLoadIdentity();
 
         glTranslatef(world.camera.x, world.camera.y, world.camera.z);
-
         glRotatef(world.camera.rotationX, 1.f, 0.f, 0.f);
         glRotatef(world.camera.rotationY, 0.f, 1.f, 0.f);
         glRotatef(world.camera.rotationZ, 0.f, 0.f, 1.f);
-        
+
+        world.drawBackground();
         world.draw();
-        
     };
 
-    void apply(sf::RenderWindow & window, int width, int height, bool showFps)
+    void apply(sf::RenderWindow &window, int width, int height, bool showFps)
     {
         draw(window);
     };
-};
 
+    void setup(sf::RenderWindow &window)
+    {
+        glEnable(GL_CULL_FACE);
+        glCullFace(GL_BACK);
+        glFrontFace(GL_CCW);
+    }
+};
 
 // SFML MISC AND SETUP
 class Engine
@@ -493,7 +649,8 @@ public:
     {
         window.clear();
 
-        if(canvas3d.enable3D){
+        if (canvas3d.enable3D)
+        {
             canvas3d.apply(window, width, height, showFPS);
         }
         canvas.apply(window, width, height, showFPS);
@@ -519,6 +676,8 @@ public:
                 sf::VideoMode(width, height),
                 windowName);
         }
+
+        canvas3d.setup(window);
 
         if (!canvas.font.loadFromFile("fonts/arial.ttf")) // HERE YOU CAN GET THE FONT
         {
@@ -655,10 +814,15 @@ int main()
         int volume = 10;
         bool fullscreen = false;
     };
+    struct u_stateGame10 // IS NOT EVEN THE SAME!!!11!! UHUU ///Video
+    {
+        int color = 0;
+    };
 
     u_stateGame1 stateGame1;
     u_stateGame2 stateGame2;
     u_stateGame3 stateGame3;
+    u_stateGame10 stateGame10;
 
     stateGame3.fullscreen = configs.fullscreen;
     stateGame3.resolutionHeight = configs.heightWindow;
@@ -1247,10 +1411,15 @@ int main()
             case 10:
                 if (deltaTime == 1)
                 {
+
                     engine.canvas.clearAll();
                     engine.canvas.text(0.1f, 0.1f, "Jogando... Enter para voltar", 16, 0, 1.f, 1.f, 1.f);
                     engine.canvas.image(0.1f, 0.3f, "assets/check_uv_map.png", 0, 0, 0, 1024, 1024, 100, 100, 128.f);
                     engine.canvas3d.enable3D = true;
+
+                    Object3D cube;
+                    cube.name = "cubo";
+                    engine.canvas3d.world.scene.add(cube);
                 }
                 if (engine.getKeyDown(sf::Keyboard::Enter))
                 {
@@ -1258,35 +1427,65 @@ int main()
                     deltaTime = 99;
                     engine.canvas3d.enable3D = false;
                 }
-                if(engine.getKeyDown(sf::Keyboard::Up)){
+                if (engine.getKeyDown(sf::Keyboard::Up))
+                {
                     engine.canvas3d.world.camera.rotationX += 5.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::Down)){
+                if (engine.getKeyDown(sf::Keyboard::Down))
+                {
                     engine.canvas3d.world.camera.rotationX -= 5.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::Left)){
+                if (engine.getKeyDown(sf::Keyboard::Left))
+                {
                     engine.canvas3d.world.camera.rotationY += 5.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::Right)){
+                if (engine.getKeyDown(sf::Keyboard::Right))
+                {
                     engine.canvas3d.world.camera.rotationY -= 5.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::W)){
+                if (engine.getKeyDown(sf::Keyboard::W))
+                {
                     engine.canvas3d.world.camera.z += 1.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::S)){
+                if (engine.getKeyDown(sf::Keyboard::S))
+                {
                     engine.canvas3d.world.camera.z -= 1.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::A)){
+                if (engine.getKeyDown(sf::Keyboard::A))
+                {
                     engine.canvas3d.world.camera.x += 1.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::D)){
+                if (engine.getKeyDown(sf::Keyboard::D))
+                {
                     engine.canvas3d.world.camera.x -= 1.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::Q)){
+                if (engine.getKeyDown(sf::Keyboard::Q))
+                {
                     engine.canvas3d.world.camera.y += 1.f;
                 }
-                if(engine.getKeyDown(sf::Keyboard::E)){
+                if (engine.getKeyDown(sf::Keyboard::E))
+                {
                     engine.canvas3d.world.camera.y -= 1.f;
+                }
+                if (engine.getKeyDown(sf::Keyboard::R))
+                {
+                    if (stateGame10.color < 7)
+                    {
+                        stateGame10.color += 1;
+                    }
+
+                    Object3D &obj = engine.canvas3d.world.scene.getObject3D("cubo");
+                    obj.texture.setColor(stateGame10.color);
+                }
+                if (engine.getKeyDown(sf::Keyboard::T))
+                {
+                    if (stateGame10.color > 0)
+                    {
+                        stateGame10.color -= 1;
+                    }
+
+                    Object3D &obj = engine.canvas3d.world.scene.getObject3D("cubo");
+                    obj.texture.setColor(stateGame10.color);
                 }
                 break;
 
@@ -1300,7 +1499,7 @@ int main()
 
             nowLoop -= fixedStep; // FINALLY OUTSIDE THE WHILE
         } // RENDER ON THE SAME THREAD AS LOGIC, +10000 BRAIN
-        
+
         engine.display();
         // PLEASE, GO READ CLEAN CODE AND GO TO A PSYCHWARD, MYSELF!!1!1
     }
