2559fd58c50cdfd77dcd1440ec5e470060b2f09a
Matheus Ernesto Dos Santos <73611965+Matheus-Ernesto@users.noreply.github.com>
2026-02-27 11:38:55 -0300

    Criar utils.cpp

---
+++ engine/utils.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+struct v2f {
+    float x = 0.f;
+    float y = 0.f;
+};
+
+struct v3f {
+    float x = 0.f;
+    float y = 0.f;
+    float z = 0.f;
+};
+
+struct v4f {
+    float x = 0.f;
+    float y = 0.f;
+    float z = 0.f;
+    float w = 0.f;
+};
